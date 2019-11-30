f0975ead82f6b585af182eb7887a977a0480cb4e
Ryan Mehri <ryan.mehri1@gmail.com>
2019-11-30 13:57:29 -0800

    Change file names

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ from flask import send_file
 # Create the application
 app = flask.Flask(__name__)
 
-# serving find.html
+# serving home.html
 @app.route('/', methods=['GET'])
 def serve_page():
-    return flask.render_template('find.html')
+    return flask.render_template('home.html')
 
 # process query
 @app.route('/process_query', methods=['POST'])
 def process_query():
     data = flask.request.form  # is a dictionary
-    location = data['some_location']
-    return flask.render_template('find.html', same=location)
+    location = data['user_input']
+    return flask.render_template('home.html', same=location)
 
 
 if __name__ == '__main__':
